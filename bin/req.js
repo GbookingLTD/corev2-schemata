f36a2ec0d11a776662bf17481c35ecc97c4359ed
swvitaliy <sw.vitaliy@gmail.com>
2018-08-30 15:24:31 +0300

    check remote response from local

---
+++ bin/req.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let Ajv = require('ajv')
+  , ajv = new Ajv
+  , rpcRequest = require('./rpcRequest').rpcRequest;
+let corev2Schemata = require('../index');
+
+let loaded = false;
+
+corev2Schemata.loadSchemas(ajv).then(function() {
+  console.info('json-schema loaded successful!');
+  loaded = true;
+}).fail(function (err) {
+  console.error('json-schema error while loaded');
+  console.error(err);
+});
+
+let _validate = function(json) {
+  if (!loaded) {
+    console.info('json-schema jet not loaded');
+    return ;
+  }
+
+  let validate = corev2Schemata.getValidateSchema('business', 'get_profile_by_id', 'response');
+  let isValid = validate(json);
+  if (!isValid) {
+    console.error('Error', validate.errors);
+  } else {
+    console.info('ok');
+  }
+};
+
+let getProfileByID = function (cracEndpoint, baseBusinessId) {
+  var params = {
+    "business": {"id": ''+baseBusinessId},
+    "with_networks": true
+  };
+  return rpcRequest('business.get_profile_by_id', params, cracEndpoint).then(function(json) {
+    _validate(json)
+  }).fail(function(err) {
+    if (err.code === -32700) {
+      _validate(err.xtra);
+    }
+  });
+};
+
+
+getProfileByID(process.env.ENDPOINT, 4000000004917).done();
---
+++ bin/rpcRequest.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var request = require('request')
+  , Q = require('q');
+
+var defaultEndpoint = 'http://apiv2.gbooking.ru/rpc';
+
+exports.rpcRequest = function (method, data, endpoint) {
+  var defer = Q.defer();
+  if (arguments.length === 1) {
+    data = method;
+    method = undefined;
+  }
+
+  var json = {
+    jsonrpc: '2.0',
+    id: 2,
+    method: method,
+    params: JSON.parse(JSON.stringify(data))
+  };
+
+  if (!json.method) {
+    throw new Error('rpc request unknown method');
+  }
+
+  var req_body = {url: endpoint || defaultEndpoint, method: 'POST', json: json};
+  request(req_body, function (error, result, body) {
+    if (error || (body && body.error))
+      defer.reject(error || (body && body.error));
+    else
+      defer.resolve(body);
+  });
+  return defer.promise;
+};
