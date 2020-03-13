de941b8c83ae7acde651f9c30caae21a27954b0c
swvitaliy <sw.vitaliy@gmail.com>
2020-03-13 12:25:03 +0300

    reserve_appointment fix start time

--- bin/test_dev.js
+++ bin/test_dev.js
@@ -72,7 +72,7 @@ require('./test')(function() {
     getNetworkData(process.env.ENDPOINT, 342),
     getNetworkDataWithBusinessInfo(process.env.ENDPOINT, 342),
     reserveAppointment(process.env.ENDPOINT, "4000000003715", "9123154",
-        "5d0a27dd73876619445a2910", "2020-03-17T17:00:00.000Z")
+        "5d0a27dd73876619445a2910", "Sun, 17 Mar 2020 09:30:00 GMT")
         .then((res) => {
           let appointmentId = undefined;
           console.info("setup appointmentId", res.result? res.result.appointment.id : "unknown");
